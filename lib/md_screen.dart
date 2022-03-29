330e029297fc99a9e371e4974357510a82faf219
TheUltimateOptimist <digitalConfidence@web.de>
2022-03-29 11:50:32 +0200

    added in app navigation via markdown

--- lib/md_screen.dart
+++ lib/md_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -14,12 +15,29 @@ class MDScreen extends StatelessWidget {
           data: markDown,
           selectable: true,
           onTapLink: (linkName, url, _) async {
-            if (url != null) {
-              await launch(url);
-            }
+            if (url == null) return;
+            if (!url.startsWith("http")) {
+              String destinationMarkdown = await getArticleMarkdown(url);
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MDScreen(
+                    markDown: destinationMarkdown,
+                  ),
+                ),
+              );
+            } 
+            else await launch(url);
           },
         ),
       ),
     );
   }
+
+  Future<String> getArticleMarkdown(String docPath) async{
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  DocumentReference docRef = firestore.doc(docPath);
+  Map<String, dynamic> docData = (await docRef.get()).data() as Map<String, dynamic>;
+  return docData["markdown"];
+}
 }
